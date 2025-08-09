adc26d52da63e3e076958dd84e77848c4b2b9395
Shani Knobel <shani.knobel@post.runi.ac.il>
2025-08-09 02:07:11 +0000

    added some function signatures, implemented monitor_init

--- plugins/sync/consumer_producer.c
+++ plugins/sync/consumer_producer.c
@@ -5,3 +5,17 @@
 
 #include "consumer_producer.h"
 
+//TODO: #7 implement consumer_producer.c functions
+const char* consumer_producer_init(consumer_producer_t* queue, int capacity);
+
+void consumer_producer_destroy(consumer_producer_t* queue);
+
+const char* consumer_producer_put(consumer_producer_t* queue, const char* item);
+
+char* consumer_producer_get(consumer_producer_t* queue);
+
+void consumer_producer_signal_finished(consumer_producer_t* queue);
+
+int consumer_producer_wait_finished(consumer_producer_t* queue);
+
+
--- plugins/sync/monitor.c
+++ plugins/sync/monitor.c
@@ -4,3 +4,35 @@
 
 #include "monitor.h"
 
+//TODO: #1 implement functions
+
+int monitor_init(monitor_t* monitor){
+    if(!monitor){
+        return -1;  
+    }    
+
+    if(pthread_mutex_init(&monitor->mutex, NULL)!=0){
+        return -1;
+    }
+
+    if(pthread_cond_init(&monitor->condition, NULL)!=0){
+        pthread_mutex_destroy(&monitor->mutex);
+        return -1;
+    }
+
+    monitor->signaled=0;
+    return 0;
+}
+
+//TODO: implement monitor_destroy
+void monitor_destroy(monitor_t* monitor);
+
+//TODO: implement monitor_signal
+void monitor_signal(monitor_t* monitor);
+
+//TODO: implement monitor_reset
+void monitor_reset(monitor_t* monitor);
+
+//TODO: implement monitor_wait
+int monitor_wait(monitor_t* monitor);
+
