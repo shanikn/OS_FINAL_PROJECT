84f2d15ac79d13d23faf7a1dd1d15fc1519e8513
Shani Knobel <shani.knobel@post.runi.ac.il>
2025-08-10 21:12:28 +0000

    finished implementing monitor.c

--- plugins/sync/monitor.c
+++ plugins/sync/monitor.c
@@ -4,18 +4,21 @@
 
 #include "monitor.h"
 
-//TODO: #1 implement functions
 
 int monitor_init(monitor_t* monitor){
+    // it's NULL
     if(!monitor){
         return -1;  
     }    
 
+    // pthread functions return 0 on success
     if(pthread_mutex_init(&monitor->mutex, NULL)!=0){
         return -1;
     }
 
+    // pthread functions return 0 on success
     if(pthread_cond_init(&monitor->condition, NULL)!=0){
+        // destroy the mutex if the condition variable is NULL
         pthread_mutex_destroy(&monitor->mutex);
         return -1;
     }
@@ -24,15 +27,59 @@ int monitor_init(monitor_t* monitor){
     return 0;
 }
 
-//TODO: implement monitor_destroy
-void monitor_destroy(monitor_t* monitor);
+void monitor_destroy(monitor_t* monitor){
+    if(!monitor){
+        return;  
+    }    
 
-//TODO: implement monitor_signal
-void monitor_signal(monitor_t* monitor);
+    // with pthread destroy functions
+    pthread_mutex_destroy(&monitor->mutex);
+    pthread_cond_destroy(&monitor->condition);
+}
 
-//TODO: implement monitor_reset
-void monitor_reset(monitor_t* monitor);
+void monitor_signal(monitor_t* monitor){
+    if(!monitor){
+        return;  
+    }    
+    pthread_mutex_lock(&monitor->mutex);
 
-//TODO: implement monitor_wait
-int monitor_wait(monitor_t* monitor);
+    // set the signal to 1
+    monitor->signaled=1;
+    pthread_cond_broadcast(&monitor->condition);
+
+    pthread_mutex_unlock(&monitor->mutex);
+    
+}
+
+void monitor_reset(monitor_t* monitor){
+    if(!monitor){
+        return;  
+    }    
+    pthread_mutex_lock(&monitor->mutex);
+
+    // reset signal to 0
+    monitor->signaled=0;
+
+    pthread_mutex_unlock(&monitor->mutex);
+}
+
+int monitor_wait(monitor_t* monitor){
+    if(!monitor){
+        return -1;  
+    }    
+    int rc=0;
+
+    pthread_mutex_lock(&monitor->mutex);
+    
+    while(!monitor->signaled){
+        if(pthread_cond_wait(&monitor->condition, &monitor->mutex)!=0){
+            pthread_mutex_unlock(&monitor->mutex);
+            return -1;
+        }
+    }
+
+    pthread_mutex_unlock(&monitor->mutex);
+    // on success
+    return 0;
+}
 
